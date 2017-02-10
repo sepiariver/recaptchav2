8e3ebcc2423b101277f778cf4458637adc4d76cf
Steeve <lawebfabric@gmail.com>
2017-02-10 17:17:31 +0100

    Update default.inc.php

--- core/components/recaptchav2/lexicon/en/default.inc.php
+++ core/components/recaptchav2/lexicon/en/default.inc.php
@@ -7,5 +7,5 @@
  *
  */
 // EXAMPLE: $_lang[''] = '';
-$_lang['technical_error_message'] = 'Sorry, there was an error submitting your form. Please use one of the contacts on this page instead.';
-$_lang['recaptcha_error_message'] = 'Please select the checkbox in the ReCaptcha image.';
+$_lang['recaptchav2.technical_error_message'] = 'Sorry, there was an error submitting your form. Please use one of the contacts on this page instead.';
+$_lang['recaptchav2.recaptcha_error_message'] = 'Please select the checkbox in the ReCaptcha image.';
