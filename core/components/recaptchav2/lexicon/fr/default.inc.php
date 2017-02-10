99b30af0e9b4e9c82e05c140e85b26ad4649b9b9
Steeve <lawebfabric@gmail.com>
2017-02-10 17:17:51 +0100

    Update default.inc.php

--- core/components/recaptchav2/lexicon/fr/default.inc.php
+++ core/components/recaptchav2/lexicon/fr/default.inc.php
@@ -7,5 +7,5 @@
  *
  */
 // EXAMPLE: $_lang[''] = '';
-$_lang['technical_error_message'] = 'Désolé, une erreur s\'est produite lors de la soumission de votre formulaire. Veuillez utiliser l\'un des contacts de cette page à la place.';
-$_lang['recaptcha_error_message'] = 'S\'il vous plaît, cochez la case : Je ne suis pas un robot.';
+$_lang['recaptchav2.technical_error_message'] = 'Désolé, une erreur s\'est produite lors de la soumission de votre formulaire. Veuillez utiliser l\'un des contacts de cette page à la place.';
+$_lang['recaptchav2.recaptcha_error_message'] = 'S\'il vous plaît, cochez la case : Je ne suis pas un robot.';
