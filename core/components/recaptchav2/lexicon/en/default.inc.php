17474c006c7c117bdbf9749a9b20c240677baa44
Steeve <lawebfabric@gmail.com>
2017-02-10 16:52:10 +0100

    Update default.inc.php

--- core/components/recaptchav2/lexicon/en/default.inc.php
+++ core/components/recaptchav2/lexicon/en/default.inc.php
@@ -6,3 +6,6 @@
  * @subpackage lexicon
  *
  */
+// EXAMPLE: $_lang[''] = '';
+$_lang['technical_error_message'] = 'Sorry, there was an error submitting your form. Please use one of the contacts on this page instead.';
+$_lang['recaptcha_error_message'] = 'Please select the checkbox in the ReCaptcha image.';
