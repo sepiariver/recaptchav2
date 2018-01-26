ea137aa1f292420eb30237808d21598a675ebaa6
gadgetto <martin.gartner@bitego.com>
2018-01-26 09:53:27 +0100

    Added german lexicon entries

---
+++ core/components/recaptchav2/lexicon/de/default.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * German Lexicon Entries for recaptchav2
+ *
+ * @package recaptchav2
+ * @subpackage lexicon
+ *
+ */
+// EXAMPLE: $_lang[''] = '';
+$_lang['recaptchav2.technical_error_message'] = 'Beim Absenden des Formulars ist ein Fehler aufgetreten. Bitte verwenden Sie stattdessen einen der Kontakte auf dieser Seite.';
+$_lang['recaptchav2.recaptcha_error_message'] = 'Bitte aktivieren Sie die Checkbox im ReCaptcha Bild.';
