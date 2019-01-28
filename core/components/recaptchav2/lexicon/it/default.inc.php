3092009c5d979cd1caef345049892d323c6b2e38
alessandro chinazzo <alessandro.chinazzo@gmail.com>
2019-01-28 12:15:52 +0100

    Italian translation

---
+++ core/components/recaptchav2/lexicon/it/default.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Italian Lexicon Entries for recaptchav2
+ *
+ * @package recaptchav2
+ * @subpackage lexicon
+ *
+ */
+// EXAMPLE: $_lang[''] = '';
+$_lang['recaptchav2.technical_error_message'] = 'Si è verificato un errore nell\'invio del modulo. Per cortesia utilizza one dei contatti in questa pagina.';
+$_lang['recaptchav2.recaptcha_error_message'] = 'Per favore spunta la checkbox nell\'immagine ReCaptcha.';
