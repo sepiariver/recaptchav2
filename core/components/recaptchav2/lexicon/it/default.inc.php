115fe0e1dc38770b0d10507802d4bb5418838380
sepiariver <info@sepiariver.com>
2019-03-30 14:04:56 -0700

    add lexicon entry

--- core/components/recaptchav2/lexicon/it/default.inc.php
+++ core/components/recaptchav2/lexicon/it/default.inc.php
@@ -9,3 +9,4 @@
 // EXAMPLE: $_lang[''] = '';
 $_lang['recaptchav2.technical_error_message'] = 'Si è verificato un errore nell\'invio del modulo. Per cortesia utilizza uno dei contatti in questa pagina.';
 $_lang['recaptchav2.recaptcha_error_message'] = 'Per favore spunta la checkbox nell\'immagine ReCaptcha.';
+$_lang['recaptchav2.recaptchav3_error_message'] = 'Si è verificato un errore.';
