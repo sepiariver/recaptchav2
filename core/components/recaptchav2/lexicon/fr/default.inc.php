4fc502867ef851d0fc2d6e4920cc29c2a587dbe4
Thomas Jakobi <thomas.jakobi@partout.info>
2021-03-26 13:24:23 +0100

    Improved french message

--- core/components/recaptchav2/lexicon/fr/default.inc.php
+++ core/components/recaptchav2/lexicon/fr/default.inc.php
@@ -9,4 +9,4 @@
 // EXAMPLE: $_lang[''] = '';
 $_lang['recaptchav2.technical_error_message'] = 'Désolé, une erreur s\'est produite lors de la soumission de votre formulaire. Veuillez utiliser l\'un des contacts de cette page à la place.';
 $_lang['recaptchav2.recaptcha_error_message'] = 'S\'il vous plaît, cochez la case : Je ne suis pas un robot.';
-$_lang['recaptchav2.recaptchav3_error_message'] = 'Désolé, une erreur.';
+$_lang['recaptchav2.recaptchav3_error_message'] = 'Erreur lors de la vérification de reCAPTCHA, veuillez réessayer.';
