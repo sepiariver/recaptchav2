7c5869c6da9480b66bdbe5945ff222fd9ced9521
Thomas Jakobi <thomas.jakobi@partout.info>
2021-03-26 13:18:35 +0100

    Improved reCAPTCHA error messages

--- core/components/recaptchav2/lexicon/en/default.inc.php
+++ core/components/recaptchav2/lexicon/en/default.inc.php
@@ -9,4 +9,4 @@
 // EXAMPLE: $_lang[''] = '';
 $_lang['recaptchav2.technical_error_message'] = 'Sorry, there was an error submitting your form. Please use one of the contacts on this page instead.';
 $_lang['recaptchav2.recaptcha_error_message'] = 'Please select the checkbox in the ReCaptcha image.';
-$_lang['recaptchav2.recaptchav3_error_message'] = 'Sorry something went wrong. Please contact the site administrator.';
+$_lang['recaptchav2.recaptchav3_error_message'] = 'Error verifying reCAPTCHA, please try again.';
